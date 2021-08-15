39d2ced881af6c3b51b64ac4ac5b91d4e82f723e
Jacques <jacques_gm@hotmail.com>
2021-08-15 11:47:05 -0300

    Evoluindo com APIs

--- Aula11/endereco/js/pegarEndereco.js
+++ Aula11/endereco/js/pegarEndereco.js
@@ -1,13 +1,21 @@
-let endereco = {
-    logradouro: 'Rua Barca Velha',
-    cidade: 'Fortaleza',
-    uf: 'CE',
-    bairro: 'Jangurussu',
-};
 
-INPUT_CEP.getElementById('cep').addEventListener('blur', () => {
+INPUT_CEP.addEventListener('blur', () => {
     if(INPUT_CEP.value.length !== 8){
         return;
     }
-    alert('buscando');
+
+    fetch(`https://viacep.com.br/ws/${INPUT_CEP.value}/json/`)
+        .then(response => response.json())
+        .then(endereco => {
+
+            if(endereco.erro === true){
+                alert('CEP Invalido!!');
+                return;
+            }
+
+            INPUT_LOGRADOURO.value = endereco.logradouro;
+            INPUT_BAIRRO.value = endereco.bairro;
+            INPUT_CIDADE.value = endereco.localidade;
+            INPUT_UF.value = endereco.uf;
+        });
 });
